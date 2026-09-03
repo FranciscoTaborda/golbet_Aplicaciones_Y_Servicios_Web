fa71d6f65103a4bb6270763aac50bba463e0d5b6
FranciscoTaborda <franciscotaborda7710@gmail.com>
2026-09-03 15:45:28 -0500

    Add generic repository, match repository and database seeder (Module 3)

---
+++ GolBet.Repositories/Interfaces/IMatchRepository.cs
@@ -0,0 +1,18 @@
+﻿using GolBet.Entities;
+using GolBet.Entities.Enums;
+using GolBet.Repositories.Data;
+using GolBet.Repositories.Implementations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolBet.Repositories.Interfaces;
+
+public interface IMatchRepository : IGenericRepository<Match>
+{
+    Task<IEnumerable<Match>> GetAllWithTeamsAsync(MatchStatus? status = null);
+    Task<Match?> GetByIdWithDetailsAsync(int id);
+}
--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,7 +1,15 @@
 using GolBet.Repositories.Data;
+using GolBet.Repositories.Implementations;
+using GolBet.Repositories.Interfaces;
+using GolBet.Repositories.Implementations;
 using Microsoft.EntityFrameworkCore;
+using GolBet.Repositories.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+builder.Services.AddScoped<IMatchRepository, MatchRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -10,6 +18,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await DbSeeder.SeedAsync(context);
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
